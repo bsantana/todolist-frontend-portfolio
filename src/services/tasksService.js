b8a6822f6e9e47b8ca308be1083f29eec378487c
Bruno Santana <bruno.felicio@fichacerta.com.br>
2025-05-03 18:46:32 -0300

    refatorando apiService para tasksService

---
+++ src/services/tasksService.js
@@ -0,0 +1,37 @@
+import axios from 'axios';
+
+const API_BASE_URL = 'http://localhost:3000';
+
+export const getListTasks = async () => {
+  try {
+    const response = await axios.get(`${API_BASE_URL}/tasks`);
+    return response.data;
+  } catch (err) {
+    console.error('Erro ao buscar tasks:', err);
+    throw err;
+  }
+}
+
+export const createTasks = async (title) => {
+  try {
+    const data = {
+      title: title
+    }
+    const response = await axios.post(`${API_BASE_URL}/tasks`, data);
+    return response.data;
+  } catch (err) {
+    console.log('Erro ao criar tasks:', err);
+    // return false;
+    throw err;
+  }
+}
+
+export const deleteTasks = async (taskId) => {
+  try {
+    const response = await axios.delete(`${API_BASE_URL}/tasks/${taskId}`);
+    return response.data;
+  } catch (err) {
+    console.error('Erro ao deletar tasks:', err);
+    throw err;
+  }
+}
